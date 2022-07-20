0ccd9eee2896ad6f2faf30dbbef538d56d33795d
Coding Gears <Adhishmeena@hotmail.com>
2022-07-20 23:43:36 +0530

    Adding actions to the project

---
+++ frontend/src/Actions/productActions.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import {
+  PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_REQUEST,
+} from "../Constants/productConstant";
+
+export const listproducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST });
+
+    const { data } = await axios.get("/api/products");
+
+    dispatch({
+      type: PRODUCT_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
